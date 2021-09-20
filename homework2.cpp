effa96135126755bb872f54db67b34361137254f
wzr <543006470@qq.com>
2021-09-20 22:05:46 +0800

    3

--- homework2.cpp
+++ homework2.cpp
@@ -7,8 +7,7 @@ int main(){
 int level;
 int i;
 int num_keyword=0;//关键词个数 
-int num_switch=0;//switch个数 
-int num_switch_case[1000]={0}; //每个switch对应的case个数 
+
 string keyword[32]={"auto","break;","case","char","const","continue","default:","double","do","else","enum","extern","float","for",
                     "goto","if","int","long","register","return","short","signed","sizeof","static","struct","switch","typedef",
                     "union","unsigned","void","volatile","while"}; //关键词库 
@@ -34,21 +33,11 @@ ifs.open(file,ios::in);
 		break;
 	}
     } //结束 
-	if(single_word.find(keyword[25])!=single_word.npos){//寻找switch数量，并且确定相应的case的数量 
-		num_switch++;
-	}else{
-		if(num_switch>=1){
-			if(single_word.find(keyword[2])!=single_word.npos)	num_switch_case[num_switch-1]++;
-    }
-	}//结束 
+	
 	
     }	
 	}
 	cout<<"total num: " <<num_keyword<<endl;
-	cout<<"switch num: "<<num_switch<<endl;
-	cout<<"case num: ";
-    for(i=0;i<num_switch;i++){
-    	cout<<num_switch_case[i]<<" ";
-	}
+
 	cout<<endl;
 				}
